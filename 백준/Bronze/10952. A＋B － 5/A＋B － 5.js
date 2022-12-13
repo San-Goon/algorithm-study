b9f5a1bceb2d29e2056248089ca3d832897b1c42
San-Goon <84852012+San-Goon@users.noreply.github.com>
2022-12-13 21:40:58 +0900

    [Bronze V] Title: A+B - 5, Time: 180 ms, Memory: 9572 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.js
@@ -0,0 +1,10 @@
+let fs = require('fs')
+let input = fs.readFileSync('dev/stdin').toString().split('\n');
+
+input.pop();
+input.pop();
+
+for (let str of input) {
+    const nums = str.split(" ");
+    console.log(Number(nums[0]) + Number(nums[1]))
+}
