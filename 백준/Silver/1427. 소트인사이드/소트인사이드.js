74e1d704337ef65175b5a9e33d2b2008a1b2666a
San-Goon <84852012+San-Goon@users.noreply.github.com>
2023-02-02 22:35:00 +0900

    [Silver V] Title: 소트인사이드, Time: 128 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.js
@@ -0,0 +1,12 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+
+function sort(number){
+  let answer = ''
+  answer = Number(number.toString().split("").sort((a,b)=>(b-a)).join(""))
+  
+  return answer
+}
+
+console.log(sort(input))
