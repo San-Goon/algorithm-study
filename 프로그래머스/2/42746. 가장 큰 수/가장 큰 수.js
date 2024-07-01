f5686c20a973d827e17bef1a6fd34d977d5b5949
San-Goon <84852012+San-Goon@users.noreply.github.com>
2024-07-01 21:56:58 +0900

    [level 2] Title: 가장 큰 수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+const answer = numbers
+	.map(numbers => String(numbers))
+    .sort((a,b) => (b+a) - (a+b))
+    .join('');
+    
+//0인 경우는 제외해야 하니 삼항연산자 사용
+    return answer[0] === '0' ? '0' : answer;
+}
